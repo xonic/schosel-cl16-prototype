5749130fdcd334d3563a7d72b43456dc5fb7c30b
Thomas Nägele <hello.thomas.naegele@gmail.com>
2016-08-26 17:45:52 +0200

    - style matches page - implement save button logic

--- src/assets/javascript/main.js
+++ src/assets/javascript/main.js
@@ -1,1 +1,11 @@
+// Select all HTML elements with class .js-input
+var inputs = document.getElementsByClassName('js-input');
 
+function formChangeHandler(){
+  document.querySelector('html').classList.add('bets-changed');
+}
+
+// Attach focus & blur handlers to selected inputs
+Array.prototype.forEach.call(inputs, function(element, index, array){
+  element.addEventListener('change', formChangeHandler);
+});
